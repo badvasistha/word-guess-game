3d0b14405ce879e19d1d7c169a76351eb54e416d
Badri Vasistha <badri.vasistha@gmail.com>
2019-05-23 21:45:01 -0700

    final files of word-guess-game

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,115 @@
-wordToGuess = "mail"
-const wordToGuessArray = wordToGuess.split("")
-console.log(wordToGuessArray)
-// get the number of characters
-let lengthOfWordToGuess = wordToGuess.length
-console.log(lengthOfWordToGuess)
+//VARIABLES
+var words = ["netflix", "hulu", "theoffice", "amazon", "instagram", "fitness", "youtube"]
+//Empty variables to store values later
+var randomWord = "";
+var lettersOfWord = [];
+var blanks = 0;
+var blanksAndCorrect = [];
+var wrongGuess = [];
 
+//Counter Variables
+var wins = 0;
+var losses = 0;
+var guessesRemaining = 9;
 
-let save = []
-document.onkeyup = (event) => {
-    let keyThatWasPressed = event.key;
-    console.log(event.key)
+//GAME START FUNCTION
+function Game() {
+    //computer generates random word from words array
+    randomWord = words[Math.floor(Math.random() * words.length)];
 
-    const result = wordToGuessArray.indexOf(keyThatWasPressed)
-    console.log (result)
+    // split the individual word into separate arrays, and store into a new array 
+    lettersOfWord = randomWord.split("");
 
-    // let save = []
-if (result > -1) {
-    // right guess
-    console.log ("right")
-    save [result] = keyThatWasPressed
-    console.log ("right", save)  
+    //store length of word in blanks, for later use
+    blanks = lettersOfWord.length;
 
-    let notComplete = false
-    wordToGuessArray.forEach((value, index) => {
-        if (save[index]!==value) {
-            notComplete = true
-        }
-    })
-    console.log(notComplete)
-    if (!notComplete) {
-        console.log("you win!")
+    //creating a loop to generate "_" for each letter in array stored in blanks
+    for (var i = 0; i < blanks; i++) {
+        blanksAndCorrect.push("_");
     }
+
+    //showing the "_" within HTML
+    document.getElementById("currentword").innerHTML = "  " + blanksAndCorrect.join("  ");
+
+    //console logging 
+    console.log(randomWord);
+    console.log(lettersOfWord)
+    console.log(blanks)
+    console.log(blanksAndCorrect)
 }
-else {
-    //wrong
-    console.log ("wrong")
+
+//RESET FUNCTION
+function reset() {
+    guessesRemaining = 9;
+    wrongGuess = [];
+    blanksAndCorrect = [];
+    Game()
 }
+
+//CHECK LETTERS/COMPARE FUNCTION
+
+//If/Else, to see if letter selected matches random word
+function checkLetters(letter) {
+    var letterInWord = false;
+    //if the generated randomword is equal to the letter entered... then variable is true
+    for (var i = 0; i < blanks; i++) {
+        if (randomWord[i] == letter) {
+            letterInWord = true;
+        }
+    }
+    //if letterInWord (false)
+    if (letterInWord) {
+        //check each letter to see if it matches word
+        for (var i = 0; i < blanks; i++) {
+            if (randomWord[i] == letter) {
+                blanksAndCorrect[i] = letter;
+            }
+        }
+    }
+    //otherwise, push the incorrect guess in the wrong guesses section, and reduce remaining guesses
+    else {
+        wrongGuess.push(letter);
+        guessesRemaining--;
+    }
+    console.log(blanksAndCorrect);
 }
-const show_win = () => {
-    document.getElementById("show_win").innerText = "Congrats!"
+
+//FINAL COMPLETE FUNCTION
+
+//check to see if player won...
+function complete() {
+    console.log("wins:" + wins + "| guesses left:" + guessesRemaining)
+
+    //if WON...then alert, and reset new round
+    if (lettersOfWord.toString() == blanksAndCorrect.toString()) {
+        wins++;
+        // aud()
+        reset()
+        //display wins on screen
+        document.getElementById("winstracker").innerHTML = " " + wins;
+
+        //if LOST...then alert and reset new round
+    } else if (guessesRemaining === 0) {
+        reset()
+    }
+    //display guesses remaining countdown
+    document.getElementById("currentword").innerHTML = "  " + blanksAndCorrect.join(" ");
+    document.getElementById("guessesremaining").innerHTML = " " + guessesRemaining;
+}
+
+//call start game function
+Game()
+
+//check for keyup, and convert to lowercase then store in guesses
+document.onkeyup = function (event) {
+    var guesses = String.fromCharCode(event.keyCode).toLowerCase();
+    //check to see if guess entered matches value of random word
+    checkLetters(guesses);
+    //process wins/loss 
+    complete();
+    //store player guess in console for reference 
+    console.log(guesses);
+
+    //display/store incorrect letters on screen
+    document.getElementById("playerguesses").innerHTML = "  " + wrongGuess.join(" ");
 }
